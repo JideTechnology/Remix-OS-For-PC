5acf088f57458f75d10d74d51deaa9409d3cad6b
Karol Putra <ventricle.vioner@gmail.com>
2016-10-20 03:48:03 +0200

    log_generator v0.6 update
    Changed paths for the logs and filename structure

--- log_generator.sh
+++ log_generator.sh
@@ -1,18 +1,18 @@
 #! /system/bin/sh
-#SET VARS FOR LOGFILE NAMING in format: <LOGTYPE>_<DEVICE MODEL>_<REMIX VERSION>_<REMIX ARCH>_<DATE>.TXT
+#SET VARS FOR LOGFILE NAMING in format: <LOGTYPE>_BOOT-COMPLETE_<DEVICE MODEL>_<REMIX VERSION>_<REMIX ARCH>_<DATE>.TXT
 model=$(getprop ro.product.model)
 version=$(getprop ro.build.remixos.version)
 system_arch=$(getprop ro.product.cpu.abi)
-logcat="logcat_${model}_${version}_${system_arch}_$(date +%F_%R).txt"
-dmesg="dmesg_${model}_${version}_${system_arch}_$(date +%F_%R).txt"
-lspci="lspci_${model}_${version}_${system_arch}_$(date +%F_%R).txt"
-lsusb="lsusb_${model}_${version}_${system_arch}_$(date +%F_%R).txt"
-cpuinfo="cpuinfo_${model}_${version}_${system_arch}_$(date +%F_%R).txt"
+logcat="logcat_boot-complete_${model}_${version}_${system_arch}_$(date +%F_%H-%M).txt"
+dmesg="dmesg_boot-complete_${model}_${version}_${system_arch}_$(date +%F_%H-%M).txt"
+lspci="lspci_boot-complete_${model}_${version}_${system_arch}_$(date +%F_%H-%M).txt"
+lsusb="lsusb_boot-complete_${model}_${version}_${system_arch}_$(date +%F_%H-%M).txt"
+cpuinfo="cpuinfo_boot-complete_${model}_${version}_${system_arch}_$(date +%F_%H-%M).txt"
 # WELCOME MESSAGE
-print "\nRemix Log Dumper v0.5\n\nLogs will be dumped into 2 locations:\n\n1. $remix_sdcard_path - Remix File Manager has /sdcard named as My Remix.\n2. /RemixOS-Logs - directory created on the storage medium RemixOS is installed on.\n\nDumping log files to $remix_sdcard_path, please wait...\n"
+print "\nRemix Log Dumper v0.6\n\nLogs will be dumped into 2 locations:\n\n1. /sdcard/RemixOS-logs - Remix File Manager has /sdcard named as My Remix.\n2. /RemixOS/RemixOS-logs - directory created on the storage medium RemixOS is installed on.\n\nDumping log files to /sdcard/RemixOS-logs, please wait...\n"
 # ACTUAL DUMPING
-# SAVING TO INTERNAL REMIX STORAGE - /sdcard/RemixOS-Logs
-remix_sdcard_path="/sdcard/RemixOS-Logs"
+# SAVING TO INTERNAL REMIX STORAGE - /sdcard/logs
+remix_sdcard_path="/sdcard/RemixOS-logs"
 mkdir -p $remix_sdcard_path
 dmesg >  $remix_sdcard_path/$dmesg
 lspci > $remix_sdcard_path/$lspci 
@@ -31,7 +31,7 @@ if  [[ $native_partition ]]; then
 		ntfs-3g $native_partition $ntfs_mountpoint 2>/dev/null
 		#if partition really accessible then copy dumps 
 		if [[ $(ls $ntfs_mountpoint 2>/dev/null) ]]; then
-			ntfs_path="$ntfs_mountpoint/RemixOS-Logs"
+			ntfs_path="$ntfs_mountpoint/RemixOS/RemixOS-logs"
 			mkdir -p $ntfs_path
 			cp $remix_sdcard_path/$dmesg $ntfs_path $remix_sdcard_path/$lspci $ntfs_path $remix_sdcard_path/$logcat $ntfs_path $remix_sdcard_path/$lsusb $ntfs_path $remix_sdcard_path/$cpuinfo $ntfs_path 2>/dev/null
 		fi
